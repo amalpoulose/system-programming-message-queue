e0e7f2b77c24c40bb16263b9ca908ed6a6dc6a06
Amal Poulose <amalpoulose1995@hotmail.com>
2017-10-24 11:07:06 +0530

    Add files via upload

---
+++ Qn-3-rxr.c
@@ -0,0 +1,47 @@
+/*3.Implement full duplex communication using one message queue?
+Hint : use different mtypes*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+struct msgbf
+{
+	long type;
+	char s[20];
+};
+
+int main(void)
+{
+	int id;
+	struct msgbf v;
+
+	id=msgget(4,IPC_CREAT|0664);
+	if(id<0)
+	{
+		perror("msgget");
+		return;
+	}
+	if(fork())
+	{
+		while(1)
+		{
+			v.type=3;
+			scanf(" %s",v.s);
+			msgsnd(id,&v,strlen(v.s)+1,0);
+		}
+	}
+	else
+	{
+		while(1)
+		{
+			msgrcv(id,&v,sizeof(v),2,0);
+			printf("Data : %s\n",v.s);
+		}
+	}
+	return 0;
+}
+        
