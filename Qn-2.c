fa51cbb94e94dd7e7e2a320c9932be8efe93c707
Amal Poulose <amalpoulose1995@hotmail.com>
2017-10-24 11:04:43 +0530

    Add files via upload

---
+++ Qn-2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+int main(void)
+{
+int id,key;
+system("ipcs -q");
+printf("enter the message queue key : ");
+scanf("%d",&key);
+id=msgget(key,0);
+if(id<0)
+  printf("\n\nMessage queue not exist\n");
+msgctl(id,IPC_RMID,0);
+printf("\n  Done\n");
+return 0;
+}
