1bf5ae9c8036bf3620143e00dc68ffeccaf37575
Amal Poulose <amalpoulose1995@hotmail.com>
2017-10-24 11:09:05 +0530

    Update Qn-1-txr.c

--- Qn-1-txr.c
+++ Qn-1-txr.c
@@ -1,3 +1,6 @@
+/* 1.process1 will send directory name to process2.
+Process2 after recieving directory name,open directory ,send back sizeof file and no.of hardlinks
+to process1. */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
